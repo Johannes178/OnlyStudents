2a4f6747afbf868b37cb55185b5bd3d4d7b4ea95
johannes <johannes.jokinen@metropolia.fi>
2022-04-26 12:27:07 +0300

    test

--- onlystudents/src/utils/variables.js
+++
@@ -1,5 +0,0 @@
-const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
-const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
-const appID = 'jennash1';
-
-export {baseUrl, mediaUrl, appID};
