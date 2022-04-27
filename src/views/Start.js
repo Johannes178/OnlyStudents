1fc9df506e399b8998fcda7effe124cb59485c03
johannes <johannes.jokinen@metropolia.fi>
2022-04-27 11:13:45 +0300

    buttoneita

--- src/views/Start.js
+++ src/views/Start.js
@@ -1,11 +1,47 @@
-import {Typography} from '@mui/material';
+import {Button} from '@mui/material';
+import {useContext, useEffect} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {MediaContext} from '../contexts/MediaContext';
+import {useUser} from '../hooks/ApiHooks';
 
 const Start = () => {
+  const {user, setUser} = useContext(MediaContext);
+  const {getUser} = useUser();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      const userData = await getUser(localStorage.getItem('token'));
+      console.log(userData);
+      setUser(userData);
+    } catch (err) {
+      setUser(null);
+      navigate('/');
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  console.log(user, open);
+
   return (
     <>
-      <Typography component="h1" variant="h2">
-        Start
-      </Typography>
+      <Button
+        component={Link}
+        to={user ? '/login' : '/login'}
+        color="primary"
+        variant="contained"
+      >
+        KIRJAUDU
+      </Button>
+      <Button
+        component={Link}
+        to={user ? '/login' : '/login'}
+        color="primary"
+        variant="contained"
+      ></Button>
     </>
   );
 };
